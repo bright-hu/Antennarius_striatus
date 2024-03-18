b32224803ed422717ab956cd5df974005e2804bc
Mingliang Hu <44389503+bright-hu@users.noreply.github.com>
2024-03-18 12:40:45 +0800

    Delete 01.jellyfish.sh

--- 01.jellyfish.sh
+++
@@ -1,3 +0,0 @@
-for i in {13,15,17,19,21,23,25,27,29,31};do
-	echo "mkdir -p K_$i;cd K_$i;jellyfish count -C -m $i -s 30000000000 -t 50 -g ../generators -o reads_$i.jf;jellyfish histo -t 50 reads_$i.jf > reads_$i.histo";
-done > ${0}.sh
