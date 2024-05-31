d0e0ad634df67625b364df4ef8322aa14c290e49
Mingliang Hu <44389503+bright-hu@users.noreply.github.com>
2024-05-31 17:10:28 +0800

    Delete 05.code/04.genome_syn directory

--- 05.code/04.genome_syn/00.last.sh
+++
@@ -1,8 +0,0 @@
-##lastdb.sh
-lastdb -P30 -c -u NEAR Antennarius_maculatus.NEAR Antennarius_maculatus.fa
-
-##lastal.sh
-lastal -i50G -P 50 -m 100 -E 0.05 Antennarius_maculatus.NEAR Biyu_hic.sm.chr.fa | last-split -m1 > temp.maf;
-
-perl transfer.pl temp.maf
-circos -conf circos.conf
